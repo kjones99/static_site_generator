02313df1abf7d90d2475464ddfabf8528c312a24
Kellan <KellanJones@gmail.com>
2025-11-03 13:30:22 -0500

    added htmlnode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,25 @@ class HTMLNode():
         self.children = children
         self.props = props
 
+    def __repr__(self):
+        node_str = ''
+        if self.tag:
+            node_str += f'tag: {self.tag}\n'
+        if self.value:
+            node_str += f'text: {self.value}\n'
+        if self.props:
+            node_str += f'{self.props_to_html()}\n'
+        if self.children:
+            node_str += 'Child HTML Nodes\n'
+            for i in range(len(self.children)):
+                node_str += f'Child 1:\n{self.children[i].__repr__()}'
+        return node_str
+    
     def to_html(self):
         raise NotImplementedError
     
-    def 
+    def props_to_html(self):
+        props_str = ''
+        for key in self.props:
+            props_str += f' {str(key)}={str(self.props[key])}'
+        return props_str
---
+++ src/test_htmlnode.py
@@ -0,0 +1,30 @@
+import unittest
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_repr(self):
+        node1 = HTMLNode('a very real tag', 'wash ufizi drive me to firenze', props={'href': 'https://www.phish.net', 'googly' : 1234})
+        node1_repr_str = 'tag: a very real tag\ntext: wash ufizi drive me to firenze\n href=https://www.phish.net googly=1234\n'
+        self.assertEqual(repr(node1), node1_repr_str)
+
+    def test_repr_with_children(self):
+        node1 = HTMLNode('<p>', 'wash ufizi drive me to firenze', props={'href': 'https://www.phish.net', 'googly' : 1234})
+        node1_repr_str = 'tag: <p>\ntext: wash ufizi drive me to firenze\n href=https://www.phish.net googly=1234\n'
+        node2 = HTMLNode('<p>', 'wash ufizi drive me to firenze', [node1],{'href': 'https://www.phish.net', 'googly' : 1234})
+        node2_repr_str = f'{node1_repr_str}Child HTML Nodes\nChild 1:\n{node1_repr_str}'
+        self.assertEqual(repr(node2), node2_repr_str)
+
+    def test_props_to_html(self):
+        node1 = HTMLNode(props={'mmorpg': 'https://www.WorldofWarcraft.com', 'myivl' : 721})
+        node2 = HTMLNode('<p>', 'wash ufizi drive me to firenze', props={'mmorpg': 'https://www.WorldofWarcraft.com', 'myivl' : 721})
+        self.assertEqual(node1.props_to_html(), node2.props_to_html())
+
+    def test_not_equal_props(self):
+        node1 = HTMLNode('<p>', 'wash ufizi drive me to firenze', props={'href': 'https://www.phish.net', 'googly' : 1234})
+        node2 = HTMLNode('<p>', 'wash ufizi drive me to firenze', props={'href': 'https://www.phish.com', 'googly' : 1234})
+        self.assertNotEqual(node1.props_to_html(), node2.props_to_html)
+
+
+if __name__ == "__main__":
+    unittest.main()
